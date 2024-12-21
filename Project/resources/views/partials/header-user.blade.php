436212c206f6228a97da435786465291cd95738e
DaiNam159 <dainam15986@gmail.com>
2024-12-21 21:06:09 +0700

    edit dropdown menu.

--- Project/resources/views/partials/header-user.blade.php
+++ Project/resources/views/partials/header-user.blade.php
@@ -35,7 +35,7 @@
                  <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
                      <span class="navbar-toggler-icon"></span>
                  </button>
-                 <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+                 <div class="collapse navbar-collapse justify-content-around" id="navbarCollapse">
                      <div class="navbar-nav mr-auto py-0">
                          <a href="/" class="nav-item nav-link active">Home</a>
                          <a href="/about" class="nav-item nav-link">About</a>
@@ -44,11 +44,32 @@
                          <a href="contact" class="nav-item nav-link">Contact</a>
                      </div>
                      @if (Route::has('login'))
-                <div class="d-flex justify-content-end">
+                <div class="d-flex justify-content-end" style="width:20%;">
                     @auth
-                    <a href="{{ url('/dashboard') }}" class="btn btn-link text-dark">Dashboard</a>
+                    <ul class="navbar-nav ms-3 d-flex">
+                        <li class="nav-item dropdown">
+                            <!-- Link đến dashboard không bị ảnh hưởng bởi dropdown -->
+                            <a class="nav-link m-0" href="/dashboard">
+                                {{ Auth::user()->name }}
+                                <i class="bi bi-chevron-down ms-2" id="dropdownIcon"></i>
+
+                            </a>
+                            <!-- Menu dropdown -->
+                            <ul class="dropdown-menu" style="right:0 !important;" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="{{ route('profile.edit') }}">{{ __('Profile') }}</a></li>
+                                <li>
+                                    <form method="POST" action="{{ route('logout') }}">
+                                        @csrf
+                                        <button class="dropdown-item" type="submit">{{ __('Log Out') }}</button>
+                                    </form>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+
+                    
                     @else
-                    <a href="{{ route('login') }}" class="btn btn-link text-light">Log in</a>
+                        <a href="{{ route('login') }}" class="btn btn-link text-light">Log in</a>
                         @if (Route::has('register'))
                         <a href="{{ route('register') }}" class="ml-4 btn btn-link text-light">Register</a>
                         @endif
